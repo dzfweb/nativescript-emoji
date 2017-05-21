12abf1f25683b9d81e2b6548c78215071e999d25
Douglas Zanco Franco <douglas.franco@dzfweb.com.br>
2017-05-21 00:01:29 -0300

    ios implementation

--- emoji.ios.ts
+++ emoji.ios.ts
@@ -1,5 +1,25 @@
-import { EmojiCommon } from "./emoji.common";
+import { iconProperty, EmojiCommon } from "./emoji.common";
+import * as utils from "tns-core-modules/utils/utils";
+declare var java: any;
+
+global.moduleMerge(EmojiCommon, exports);
 
 export class Emoji extends EmojiCommon {
+  [iconProperty.getDefault](): number {
+    return 0;
+  }
+  [iconProperty.setNative](value: number) {
+    this.nativeView.text = value;
+  }
 
+  private _ios: UILabel;
+
+  get ios() {
+    return this.nativeView;
+  }
+
+  public createNativeView() {
+    this._ios = UILabel.alloc().init();
+    return this._ios;
+  }
 }
